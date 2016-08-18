ae2767ed9691aa911957403d1b2dd34857b710e4
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-08-18 10:56:24 -0600

    before working on testing in class

--- test/features/create_a_new_task_edit_delete_and_go_back_to_task_manager.rb
+++ test/features/create_a_new_task_edit_delete_and_go_back_to_task_manager.rb
@@ -1,1 +1,38 @@
 require_relative "../test_helper"
+
+class CreateANewTaskEditDeleteAndGoBack < FeatureTest
+
+  def create_task
+    visit '/tasks/new'
+    fill_in('task[title]', :with => "Title1")
+    fill_in('task[description]', :with => "Description1")
+    click_button('submit')
+  end
+
+
+  def test_create_new_task_and_edit
+    create_task
+
+    visit '/tasks/1'
+    assert page.has_content?("Title1")
+  end
+
+  def test_edit_new_task
+    create_task
+    visit '/tasks'
+    click_link('Edit')
+    fill_in('task[title]', :with => "New Title")
+    fill_in('task[description]', :with => "New Description")
+    click_button('submit')
+    assert page.has_content?("New Title")
+  end
+
+  def test_delete_a_new_task
+    create_task
+    visit '/tasks/1'
+    click_button('delete')
+    assert page.has_content?('All Tasks')
+    refute page.has_content?('Title1')
+  end
+
+end
--- test/models/task_manager_test.rb
+++ test/models/task_manager_test.rb
@@ -4,15 +4,19 @@ class TaskManagerTest < Minitest::Test
 
   include TestHelpers
 
+  def current_task_id
+    task_manager.all.last.id
+  end
+
   def test_it_creates_a_task
     task_manager.create({
       :title => "a title",
       :description => "a description"
       })
-    task = task_manager.find(1)
+    task = task_manager.find(current_task_id)
     assert_equal "a title", task.title
     assert_equal "a description", task.description
-    assert_equal 1, task.id
+    assert_equal current_task_id, task.id
   end
 
 
